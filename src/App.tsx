7dd50dae0dca6fb0e3202d89aab289c41c38a063
Marco Andrade <alonso_ac93@hotmail.com>
2024-06-10 14:05:14 -0600

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { styled } from '@mui/material';
 import './App.css';
+
+const Title = styled('h1')({
+  fontSize: 40,
+  color: 'red',
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Title>
+          Holi!
+        </Title>
         <a
           className="App-link"
           href="https://reactjs.org"
