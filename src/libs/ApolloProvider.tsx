f04f282455b84dd63c6d1afcb37af97f158a171a
Marco Andrade <alonso_ac93@hotmail.com>
2024-07-05 12:02:21 -0600

    adding apollo url env var

--- src/libs/ApolloProvider.tsx
+++ src/libs/ApolloProvider.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider as ApolloClientProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.REACT_APP_APOLLO_URL,
   cache: new InMemoryCache(),
 });
 
